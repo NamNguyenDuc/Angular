d3f99aa1a9b8feffa91e721338409eaf2c2a8f71
namnd <namnd@thinhtientech.com>
2020-11-14 11:40:45 +0700

    hihi

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,31 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "app works!";
 
   withBorder = true;
 
-  textColor = 'tomato';
+  textColor = "tomato";
 
-  onButtonClick(){
+  public editorValue: string = "";
+
+  onButtonClick() {
     this.withBorder = !this.withBorder;
-    this.title = 'Changed';
+    this.title = "Changed";
   }
 
-  onTextMouseOver(){
-    this.textColor = 'dodgerblue'
+  onTextMouseOver() {
+    this.textColor = "dodgerblue";
   }
 
   onTextMouseOut() {
-    this.textColor = 'gray'
+    this.textColor = "gray";
   }
 
-  onButtonClickedFromHello(event: string){
+  onButtonClickedFromHello(event: string) {
     this.title = event;
   }
 }
--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,25 +1,19 @@
-import { DataService } from './services/data.service';
-import { HelloComponent } from './components/hello/hello.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { DataService } from "./services/data.service";
+import { HelloComponent } from "./components/hello/hello.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
-import { AppComponent } from './app.component';
-import { HiComponent } from './components/hi/hi.component';
+import { AppComponent } from "./app.component";
+import { HiComponent } from "./components/hi/hi.component";
+
+import { CKEditorModule } from "ngx-ckeditor";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    HiComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  declarations: [AppComponent, HelloComponent, HiComponent],
+  imports: [CKEditorModule, BrowserModule, FormsModule, HttpModule],
   providers: [DataService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
